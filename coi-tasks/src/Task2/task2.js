b182dc01228b758434190fe0109ad4c6a453d32f
Sasa Nedic <svetasmak0@gmail.com>
2025-06-25 19:23:18 +0200

    Working on details imgs

--- coi-tasks/src/Task2/task2.js
+++ coi-tasks/src/Task2/task2.js
@@ -26,9 +26,10 @@ class RecipesSection extends HTMLElement {
 
     header.append(title, subtitle, button);
 
-    const createSingleCart = ({ imgSrc, imgAlt, titleText }) => {
+    const createSingleCart = ({ imgSrc, imgAlt, titleText, detailImg }) => {
       const singleCart = document.createElement("div");
       singleCart.className = "recipes-section__single-cart";
+      singleCart.style.position = "relative";
 
       const img = document.createElement("img");
       img.className = "recipes-section__img";
@@ -39,32 +40,60 @@ class RecipesSection extends HTMLElement {
       imgTitle.className = "recipes-section__cart-wrapper-title";
       imgTitle.textContent = titleText;
 
-      singleCart.append(img, imgTitle);
+      singleCart.appendChild(img);
+
+      if (detailImg && Array.isArray(detailImg)) {
+        detailImg.forEach((detail) => {
+          const wrapper = document.createElement("div");
+          wrapper.className = `recipes-section__img-wrapper ${detail.class}`;
+
+          const detailEl = document.createElement("img");
+          detailEl.className = "recipes-section__detail";
+          detailEl.src = detail.src;
+          detailEl.alt = "";
+
+          wrapper.appendChild(detailEl);
+          singleCart.appendChild(wrapper);
+        });
+      }
+
+      singleCart.appendChild(imgTitle);
       return singleCart;
     };
 
     const cardsData = [
       {
-        imgSrc: "/coctail-first.jpg",
+        imgSrc: "../../public/coctail-first.jpg",
         imgAlt: "Coctail photo 1",
         titleText: "Sparkling Triple Citrus & Mint Mocktail",
-        detailImg: [{ src: "/first-coctail-detail.png", class: "top-left" }],
-      },
-      {
-        imgSrc: "/coctail-second.png",
-        imgAlt: "Coctail photo 2",
-        titleText: "Classic Mojito Cocktail",
         detailImg: [
-          { src: "/second-coctail-detail-top.png", class: "top-left" },
-          { src: "/second-coctail-detail-bottom.png", class: "bottom-right" },
+          { src: "../../public/first-coctail-detail.png", class: "top-left" },
         ],
       },
       {
-        imgSrc: "/coctail-threed.png",
+        imgSrc: "../../public/coctail-second.png",
+        imgAlt: "Coctail photo 2",
+        titleText: "Classic Mojito Cocktail",
+        detailImg: [
+          {
+            src: "../../public/second-coctail-detail-top.png",
+            class: "top-right",
+          },
+          {
+            src: "../../public/second-coctail-details-bottom.png",
+            class: "bottom-left",
+          },
+        ],
+      },
+      {
+        imgSrc: "../../public/coctail-threed.png",
         imgAlt: "Coctail photo 3",
         titleText: "Tropical Pineapple Punch",
         detailImg: [
-          { src: "/threed-coctail-bottom-right.png", class: "bottom-right" },
+          {
+            src: "../../public/threed-coctail-bottom-right-detail.png",
+            class: "bottom-right",
+          },
         ],
       },
     ];
